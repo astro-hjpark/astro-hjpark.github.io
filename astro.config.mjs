279f5d3c1d66049ed2e77710d1618b2ecd4f7238
Hye-Jin Park <68727275+astro-hjpark@users.noreply.github.com>
2025-12-25 20:20:01 +0100

    Remove base path from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-hjpark.github.io',
-  base: '/my-repo',
   integrations: [mdx(), sitemap(), tailwind()]
 });
